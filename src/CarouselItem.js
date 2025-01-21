8e6c50eabe90e882e1cae9396ce590cb1921dc92
CharlieRonchetti <85499184+CharlieRonchetti@users.noreply.github.com>
2025-01-21 18:26:38 +0000

    center carousel text content

--- src/CarouselItem.js
+++ src/CarouselItem.js
@@ -6,14 +6,14 @@ const CarouselItem = ({ title, description, img, id, handleAnimeClick }) => {
 
   return (
     <div className='align-top inline-flex items-center lg:py-6 text-slate-50' style={{ width: "100%" }}>
-        <div className="flex flex-col-reverse gap-4 lg:flex-row">
+        <div className="flex flex-col-reverse gap-4 lg:flex-row items-center">
             <div className="flex-col basis-5/6 max-w-9/10 ml-[5%] lg:ml-0 text-center lg:text-left">
                 <h1 className="text-4xl font-semibold whitespace-normal line-clamp-3 sm:text-6xl lg:text-4xl xl:line-clamp-none xl:text-5xl">{title}</h1>
                 <p 
                   className="whitespace-normal pr-[0.5rem] hover:overflow-auto scrollbar mt-5 line-clamp-6 lg:line-clamp-4 xl:mt-10 2xl:line-clamp-[9] text-gray-400"
                   dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
                 ></p>
-                <button className="hidden ml-0.5 mt-5 bg-violet-600 py-3 px-4 rounded-md text-4xl lg:block xl:mt-10 cursor-pointer" onClick={() => handleAnimeClick(id)}>Play Now!</button>
+                <button className="hidden ml-0.5 mt-5 bg-violet-600 py-3 px-4 mb-4 rounded-md text-4xl lg:block xl:mt-10 cursor-pointer" onClick={() => handleAnimeClick(id)}>Play Now!</button>
             </div>
             <div className="">
               <img className="aspect-video object-cover max-h-full max-w-full lg:rounded-md" src={img} alt="Logo"/>
